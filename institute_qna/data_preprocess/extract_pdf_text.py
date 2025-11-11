df2e04c7f5bb5eccebf8bd37dde1d48152cf6dcd
AniketJawale17 <aniketjawale17@gmail.com>
2025-11-11 11:58:11 +0530

    updated code with ocr engine

--- institute_qna/data_preprocess/extract_pdf_text.py
+++ institute_qna/data_preprocess/extract_pdf_text.py
@@ -1,10 +1,5 @@
 
-import logging
-from unittest import loader
-from langchain_community.document_loaders import PyPDFLoader
-from pathlib import Path
-from typing import List
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader,UnstructuredPDFLoader
 from pathlib import Path
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
@@ -13,7 +8,7 @@ class PDFTextExtractor:
     """Utility class to extract text from PDF files."""
 
     @staticmethod
-    def extract_text_from_pdf(
+    def extract_text_from_text_pdf(
             pdf_path: str, 
             chunk_size: int = 2000, 
             chunk_overlap: int = 200
@@ -28,6 +23,42 @@ class PDFTextExtractor:
             text_pages: list[str] = []
             try:
                 loader = PyPDFLoader(str(path))
+                pages = loader.load()
+            except Exception as e:
+                raise RuntimeError("No PDF extraction backend available") from e
+            
+            # Step 2: Split pages into smaller chunks
+            splitter = RecursiveCharacterTextSplitter(
+                chunk_size=chunk_size,
+                chunk_overlap=chunk_overlap,
+                length_function=len,
+                separators=["\n\n", "\n", ".", " ", ""]
+            )
+
+            chunks = splitter.split_documents(pages)
+
+            print(f"✅ Split {pdf_path} into {len(chunks)} text chunks")
+            # print(chunks[0].page_content[:300])
+
+            
+            return chunks
+    
+    @staticmethod
+    def extract_text_from_hybrid_pdf(
+            pdf_path: str, 
+            chunk_size: int = 2000, 
+            chunk_overlap: int = 200
+        ) -> list[str]:
+            """Extract text from a PDF file and split it into pages then chunks.
+            """
+
+            path = Path(pdf_path)
+            if not path.exists():
+                raise FileNotFoundError(pdf_path)
+                
+            text_pages: list[str] = []
+            try:
+                loader = UnstructuredPDFLoader(str(path))
                 pages = loader.load()
             except Exception as e:
                 raise RuntimeError("No PDF extraction backend available") from e
